4b0a19f6104eccc57a2f394b3ba7adb80fe63b4b
qdosgit5 <qdosgit4+5@gmail.com>
2026-04-23 14:26:02 +0000

    Done. 69

--- fault_calc.py
+++ fault_calc.py
@@ -5,42 +5,26 @@ import math
 
 def main():
 
-    S = (0.55, 0.42, 0.25, 0.24, 0.18, 0.17, 0.14, 0.12, 0.11, 0.11, 0.06)
+    ##  Probabilities in independent probability spaces.
 
-    S = (0.1, 0.2, 0.3)
+    R = (0.55, 0.42, 0.25, 0.24, 0.18, 0.17, 0.14, 0.12, 0.11, 0.11, 0.06)
 
+    S = tuple(map(
+        lambda r: r / sum(R),
+        R
+    ))
+    
+    print(S)
+
+    print(f"New probability space: {sum(S)}")
+
+    # S = (0.1, 0.2, 0.3)
+    
     n = 2
 
-    all_combs = ()
+    all_combs = gen_all_combs(S)
 
-    for i in range(1, len(S)):
-
-        all_combs = all_combs + tuple(combinations(S, i))
-
-    product = 0
-
-    all_events = ()
-
-    ##  To calculate the probability of A n B in a probability space
-    ##  {A, B, C}, take the product of {A, B, ~C}.
-
-    for comb in all_combs:
-
-        print(comb)
-
-        comb_inv = negate_probabilities(
-            tuple(set(S) - set(comb))
-            )
-
-        event = comb + comb_inv
-
-        all_events = all_events + (event,)
-
-        print(event)
-
-        print("\n\n")        
-
-        product += math.prod(event)
+    all_events = gen_all_events(S, all_combs)
 
     if len(all_events) == len(all_combs):
 
@@ -51,14 +35,44 @@ def main():
                     )
             ))
 
-        print(p)
-
     else:
 
         print("Event calculation error")
 
     print(f"Final probability: {p}")
 
+    
+def gen_all_combs(S: tuple[float]) -> tuple[tuple[float]]:
+
+    all_combs = ()
+
+    for i in range(1, len(S)):
+
+        all_combs = all_combs + tuple(combinations(S, i))
+
+    return all_combs
+
+
+def gen_all_events(S: tuple[float], all_combs: tuple[tuple[float]]) -> tuple[tuple[float]]:
+
+    all_events = ()
+
+    ##  To calculate the probability of A n B in a probability space
+    ##  {A, B, C}, take the product of {A, B, ~C}.
+
+    for comb in all_combs:
+
+        comb_inv = negate_probabilities(
+            tuple(set(S) - set(comb))
+            )
+
+        event = comb + comb_inv
+
+        all_events = all_events + (event,)
+
+    return all_events
+
+        
 
 def negate_probabilities(s: tuple[int]):
 
